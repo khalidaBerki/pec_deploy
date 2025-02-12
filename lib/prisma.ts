2071d885fad1cf48262d47cac2294099c143dd24
khalidaBerki <112495253+khalidaBerki@users.noreply.github.com>
2025-02-12 20:57:38 +0100

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export default prisma;
-=======
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-export default prisma;
->>>>>>> 1e330dfb07b3c1100addbad2ac5c63be5485e4cf
