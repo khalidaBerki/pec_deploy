395b27012a0a18d88e56ae8ab9335d965f8a61a0
khalidaBerki <112495253+khalidaBerki@users.noreply.github.com>
2025-02-12 20:53:39 +0100

    Update page.tsx

--- src/app/dashboard/lists/products/page.tsx
+++ src/app/dashboard/lists/products/page.tsx
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-import Products from "@/components/lists/products";
-
-
-import { Metadata } from "next";
-import DefaultLayout from "@/components/Layouts/DefaultLaout";
-
-export const metadata: Metadata = {
-  title: "Next.js Tables Page | NextAdmin - Next.js Dashboard Kit",
-  description: "This is Next.js Tables page for NextAdmin Dashboard Kit",
-};
-
-const TablesPage = () => {
-  return (
-    <DefaultLayout>
-      <Breadcrumb pageName="Liste produits" />
-
-      <div className="flex flex-col gap-10">
-        <Products />
-
-      </div>
-    </DefaultLayout>
-  );
-};
-
-export default TablesPage;
-=======
 "use client";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import Products from "@/components/lists/products";
@@ -57,4 +29,3 @@ const TablesPage = () => {
 };
 
 export default TablesPage;
->>>>>>> 1e330dfb07b3c1100addbad2ac5c63be5485e4cf
