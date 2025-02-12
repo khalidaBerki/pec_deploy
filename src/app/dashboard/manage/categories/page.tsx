ccf081d9da89ee57447bde810c8828428bde9f60
khalidaBerki <112495253+khalidaBerki@users.noreply.github.com>
2025-02-12 20:55:10 +0100

    Update page.tsx

--- src/app/dashboard/manage/categories/page.tsx
+++ src/app/dashboard/manage/categories/page.tsx
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import FormCategories from "@/components/FormElements/categories";
-import { Metadata } from "next";
-import DefaultLayout from "@/components/Layouts/DefaultLaout";
-
-export const metadata: Metadata = {
-  title: "Next.js Form Elements Page | NextAdmin - Next.js Dashboard Kit",
-  description: "This is Next.js Form Elements page for NextAdmin Dashboard Kit",
-};
-
-const FormElementsPage = () => {
-  return (
-    <DefaultLayout>
-      <FormCategories/>
-    </DefaultLayout>
-  );
-};
-
-=======
 "use client";
 import React, { useEffect, useState } from "react";
 import FormCategories from "@/components/FormElements/categories";
@@ -51,5 +31,4 @@ const FormElementsPage = () => {
   );
 };
 
->>>>>>> 1e330dfb07b3c1100addbad2ac5c63be5485e4cf
 export default FormElementsPage;
